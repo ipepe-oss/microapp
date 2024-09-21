cd41b128bb8e8aa3cfac4c78a8464b43b99db270
Patryk Ptasinski <patryk@ipepe.pl>
2024-09-21 20:31:43 +0200

    Initial commit

---
+++ lib/microapp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "microapp/version"
+
+module Microapp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/microapp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Microapp
+  VERSION = "0.1.0"
+end
